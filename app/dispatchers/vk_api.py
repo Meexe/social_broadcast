c4f73c14d40b168ec3f6ff3e93f265b5afe5207f
Maxim Pak <maxpak99@gmail.com>
2020-04-14 18:05:39 +0300

    added README and requirements

--- app/dispatchers/vk_api.py
+++ app/dispatchers/vk_api.py
@@ -2,7 +2,7 @@ import requests
 import json
 
 
-URL = 'https://api.vk.com/method/wall.post'
+URL = 'https://api.vk.com/method/'
 
 
 def post(token, gid, message):
@@ -13,9 +13,53 @@ def post(token, gid, message):
         'message': message,
         'v': 5.103
     }
-    req = requests.post(URL, params=body)
+    req = requests.post(URL + 'wall.post', params=body)
     res = json.loads(req.text)
     try:
         return res['error']['error_msg']
+    except KeyError:
+        return 'success'
+
+
+def postPhoto(token, gid, message, file):
+    body = {
+        'group_id': gid[1:],
+        'access_token': token,
+        'v': 5.103
+    }
+    req = requests.post(URL + 'photos.getWallUploadServer', params=body)
+    try:
+        server = json.loads(req.text)['response']['upload_url']
+        file = {
+            'photo': open(file, 'rb')
+        }
+        req = requests.post(server, files=file)
+        body = json.loads(req.text)
+
+        body = {
+            'server': body['server'],
+            'hash': body['hash'],
+            'photo': body['photo'],
+            'group_id': gid[1:],
+            'access_token': token,
+            'v': 5.103
+        }
+        req = requests.post(URL + 'photos.saveWallPhoto', params=body)
+        media_id = json.loads(req.text)['response'][0]['id']
+        print(req.text)
+        print(media_id)
+    except KeyError:
+        return 'Error uploading file'
+    body = {
+        'owner_id': gid,
+        'message': message,
+        'attachments': 'photo' + gid + '_' + str(media_id),
+        'access_token': token,
+        'v': 5.103
+    }
+    req = requests.post(URL + 'wall.post', params=body)
+    print(req.text)
+    try:
+        return json.loads(req.text)['error']['error_msg']
     except KeyError:
         return 'success'
--- app/main.py
+++ app/main.py
@@ -5,8 +5,6 @@ import dispatchers.vk_api as vk
 import dispatchers.fb_api as fb
 from settings import settings
 
-# result = vk.post(settings['vk_token'], settings['vk_gid'], 'baca')
-# result = fb.post(settings['fb_token'], settings['fb_gid'], 'baca')
 
 class App(QtWidgets.QMainWindow, design.Ui_MainWindow):
     def __init__(self):
